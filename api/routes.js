36e65773ecdc1aa754d1b09147142fd14100ae0a
Alessio Ferrari <nate3rdz@gmail.com>
2021-07-22 12:21:54 +0200

    upload

--- api/routes.js
+++ api/routes.js
@@ -3,10 +3,9 @@ var router = express.Router();
 const https = require('https');
 
 //access token è il token di accesso a lunga durata, gettato manualmente e immesso nel codice; esso viene refreshato dal codice in via automatica
-const access_token = 'IGQVJYaFg1Yk9GNko0V29VeE9zdl9hWmR3OXJZANVk0WjNhSTFmajRHeHpUSjc2RDRKcnhRYk9ueUFrU2RDdldSWVBMVkpUR3VqNWdxbmlVZAFFtNzVwR1BoUEZAjRUw3M196SkpjQTJ3';
+const access_token = '';
 const Refresh_URL = 'https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token='+access_token;
 const URL_GetUserImages = 'https://graph.instagram.com/me/media?fields=id,media_url&access_token='+access_token;
-//const secret = 'aded4623be5c6a2ee1e34105aadad2e9';
 
 
 router.get('/getInstagramToken', (req, res) => {
@@ -45,8 +44,6 @@ router.get('/getInstagramImages', (req, res) => {
 
     }
 });
-
-//qui fai il get dei dati da mongo per visualizzare il token e se c'è bisogno di refresharlo
 
 
 
